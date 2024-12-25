419280af5e35188dea1cc02fe054da0f92bb3fe6
lilla62h <lilla62hahnrhy@hotmail.com>
2024-12-25 13:09:23 +0200

    basic sentances

---
+++ basics.js
@@ -0,0 +1,10 @@
+// Log a message to the console
+console.log("Hello, World!");
+
+// Variables
+let name = "Alice";
+const age = 25;
+let height = 5.5;
+
+// Display variables
+console.log(`My name is ${name}, I'm ${age} years old, and I'm ${height} feet tall.`);
