9b8f128e5cce89f6dad9763c2d9e9d85a687bedf
lilla62h <lilla62hahnrhy@hotmail.com>
2024-12-25 13:08:45 +0200

    Initial commit

---
+++ cycles.js
@@ -0,0 +1,11 @@
+// Print numbers 1 to 5
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+// Count down from 5 to 1
+let count = 5;
+while (count > 0) {
+    console.log(count);
+    count--;
+}
